832c42615f153cc5eb2b895c6fc49a956ba37714
HENRY <azerckid@gmail.com>
2021-06-07 17:52:28 +0900

    hashes

---
+++ blockchain/blockchain.go
@@ -0,0 +1,57 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type block struct {
+	Data     string
+	Hash     string
+	PrevHash string
+}
+
+type blockchain struct {
+	Blocks []*block
+}
+
+var b *blockchain
+var once sync.Once
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockchain().Blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().Blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.Blocks = append(b.Blocks, createBlock(data))
+}
+
+func GetBlockchain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis Block")
+		})
+	}
+	return b
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.Blocks
+}
--- main.go
+++ main.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/zizimoos/gocoin/blockchain"
 )
 
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b *blockchain) listBlocks() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("PrevHash: %s\n\n", block.prevHash)
-	}
-}
-
 func main() {
-	chain := blockchain{}
-	chain.addBlock("GenesisBlock")
-	chain.addBlock("SecondBlock")
-	chain.addBlock("ThirdBlock")
-	chain.listBlocks()
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("data : %s\n", block.Data)
+		fmt.Printf("hash : %s\n", block.Hash)
+		fmt.Printf("prevHash : %s\n\n", block.PrevHash)
+	}
 }
