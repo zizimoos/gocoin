008717ff3ce9e629a76236cb21fd5c6619f3482c
HENRY <azerckid@gmail.com>
2021-06-07 18:38:02 +0900

    http.ListenAndServe

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/zizimoos/gocoin/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello from Home")
+}
+
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("data : %s\n", block.Data)
-		fmt.Printf("hash : %s\n", block.Hash)
-		fmt.Printf("prevHash : %s\n\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+
+	// chain := blockchain.GetBlockchain()
+	// chain.AddBlock("Second Block")
+	// chain.AddBlock("Third Block")
+	// chain.AddBlock("Fourth Block")
+	// for _, block := range chain.AllBlocks() {
+	// 	fmt.Printf("data : %s\n", block.Data)
+	// 	fmt.Printf("hash : %s\n", block.Hash)
+	// 	fmt.Printf("prevHash : %s\n\n", block.PrevHash)
+	// }
 }
