f335ef771649d44e8fa74686b7847f491979c507
HENRY <azerckid@gmail.com>
2021-06-08 14:01:09 +0900

    return json

---
+++ explorer/explorer.go
@@ -0,0 +1,50 @@
+package explorer
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/zizimoos/gocoin/blockchain"
+)
+
+var templates *template.Template
+
+const (
+	port        string = ":4000"
+	templateDir string = "explorer/templates/"
+)
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(rw, "Hello from Home")
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
--- main.go
+++ main.go
@@ -1,47 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 
-	"github.com/zizimoos/gocoin/blockchain"
+	"github.com/zizimoos/gocoin/utils"
 )
 
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
+const port string = ":4000"
 
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
 }
 
-func home(rw http.ResponseWriter, r *http.Request) {
-	// fmt.Fprint(rw, "Hello from Home")
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s\n", b)
 }
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http://localhost%s\n", port)
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
-
-	// chain := blockchain.GetBlockchain()
-	// chain.AddBlock("Second Block")
-	// chain.AddBlock("Third Block")
-	// chain.AddBlock("Fourth Block")
-	// for _, block := range chain.AllBlocks() {
-	// 	fmt.Printf("data : %s\n", block.Data)
-	// 	fmt.Printf("hash : %s\n", block.Hash)
-	// 	fmt.Printf("prevHash : %s\n\n", block.PrevHash)
-	// }
 }
