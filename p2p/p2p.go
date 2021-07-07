2cf4c17c87fc68f63b70141a2ab227c93489c6d6
zizimoos_iMac <zizimoos@gmail.com>
2021-07-07 23:39:28 +0900

    cleanUp

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/zizimoos/gocoin/utils"
@@ -19,11 +20,15 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, ip, openPort)
+	peer := initPeer(conn, ip, openPort)
+	time.Sleep(20 * time.Second)
+	peer.inbox <- []byte("hello from 3000!!!")
 }
 
 func AddPeer(address, port, openPort string) {
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	initPeer(conn, address, port)
+	peer := initPeer(conn, address, port)
+	time.Sleep(10 * time.Second)
+	peer.inbox <- []byte("hello from 4000!!!")
 }
--- p2p/peer.go
+++ p2p/peer.go
@@ -9,14 +9,51 @@ import (
 var Peers map[string]*peer = make(map[string]*peer)
 
 type peer struct {
-	conn *websocket.Conn
+	key     string
+	address string
+	port    string
+	conn    *websocket.Conn
+	inbox   chan []byte
+}
+
+func (p *peer) close() {
+	p.conn.Close()
+	delete(Peers, p.key)
+}
+
+func (p *peer) read() {
+	defer p.close()
+	for {
+		_, m, err := p.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Printf("%s", m)
+	}
+}
+
+func (p *peer) write() {
+	defer p.close()
+	for {
+		m, ok := <-p.inbox
+		if !ok {
+			break
+		}
+		p.conn.WriteMessage(websocket.TextMessage, m)
+	}
 }
 
 func initPeer(conn *websocket.Conn, address, port string) *peer {
-	p := &peer{
-		conn,
-	}
 	key := fmt.Sprintf("%s:%s", address, port)
+	p := &peer{
+		conn:    conn,
+		inbox:   make(chan []byte),
+		address: address,
+		port:    port,
+		key:     key,
+	}
+	go p.read()
+	go p.write()
 	Peers[key] = p
 	return p
 }
