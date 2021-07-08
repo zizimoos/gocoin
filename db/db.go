cd3d5be26bbda2c2b08274255c2ca29830e5631e
zizimoos_iMac <zizimoos@gmail.com>
2021-07-08 23:56:21 +0900

    all blocks

--- db/db.go
+++ db/db.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/zizimoos/gocoin/utils"
 	bolt "go.etcd.io/bbolt"
 )
@@ -8,15 +11,20 @@ import (
 var db *bolt.DB
 
 const (
-	dbName       = "blockchain.db"
+	dbName       = "blockchain"
 	dataBucket   = "data"
 	blocksBucket = "blocks"
 	checkpoint   = "checkpoint"
 )
 
+func getDbName() string {
+	port := os.Args[2][6:]
+	return fmt.Sprintf("%s_%s.db", dbName, port)
+}
+
 func DB() *bolt.DB {
 	if db == nil {
-		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		dbPointer, err := bolt.Open(getDbName(), 0600, nil)
 		db = dbPointer
 		utils.HandleErr(err)
 		err = db.Update(func(t *bolt.Tx) error {
